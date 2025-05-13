85b0b966e94c25d09321ff433ad7a8020b8aeb88
오민석 <dhalstjr03@gmail.com>
2025-05-13 17:13:17 +0900

    전역변수의 중요성 ->  스크롤 이벤트 바깥에서 갱신 후, 안에서도 값을 갱신할 수 있기 때문에

--- js/header.js
+++ js/header.js
@@ -123,16 +123,29 @@ $(function () {
     }
   });
 
+  // 2. 스크롤 위치(위 아래)에 따라 헤더의 변화
+
   // 이전 스크롤값을 변수로 저장 (let)
   // 이전 스크롤값을 변수로 저장하는 이유는 스크롤 방향을 판단하기 위해 이전 스크롤값을 기억해두는 것이다.
   // 이가 필요한 이유는 현재 스크롤값만 가지고 방향을 알 수가 없기 때문에 이다.
   // 즉, 방향을 판단하기 위해서는 이전 스크롤 값을 알아야만 방향을 알 수가 있다. -> 이전 스크롤 값이 100이라면 현재 스크롤 값이 200이라고 치면 계산을 하여 음수, 양수가 나올텐데 그걸 통해서 방향을 판단하고 효과를 줄 수 있을 것이다.
   // 0으로 저장한 이유는 페이지가 처음 열릴 때 아직 스크롤하지 않았다면 이전 위치는 무조건 0이기 때문에 0으로 저장한 것이다.
-  // 1) 스크롤 업다운에 따른 변화
+  // 2-1 스크롤 업다운에 따른 변화
   let prevScrollTop = 0;
 
-  // 창 내부에 스크롤 이벤트 구현
-  window.addEventListener("scroll", function (e) {
+  // 2-2 스크롤 방향을 판단하는 함수
+  // 스크롤 방향은 값만 가지고 판단할 수 없다. -> 조건문을 활용하여 방향을 판단.
+
+  let wheelMove = function () {
+    // return는 함수가 실행될 때 결과값을 반환하는 명령어
+    // 이 함수는 결과값이 'up' , "down" 이 있기에 둘 중 하나만 결과값으로 도출한다.
+    // ?는 삼향 연산자를 말하고, 삼향 연산자는 (조건식 ? "A" : "B") if/else문법을 짧게 사용한 문법이다.
+    // 삼향 연산자를 사용하기 적합할 때는 결과가 하나이 값만을 반환할 때와, 짧고 명확한 조건일 때 사용되는 것이 좋다.
+    return prevScrollTop - nowScrollTop > 0 ? "up" : "down";
+  };
+
+  // 2-3 창 내부에 스크롤 이벤트 구현하여 클래스 부여
+  window.addEventListener("scroll", function () {
     // 이렇게 하면 스크롤의 Y의 값을 알 수 있다.
     // console.log(window.scrollY);
     // 그래서 이걸 현재 스크롤값으로 변수로 저장한다.
@@ -146,18 +159,37 @@ $(function () {
       window.pageYOffset || document.documentElement.scrollTop();
     // console.log(nowScrollTop);
 
+    // 2-4 일단 스크롤이 맨 위(0)가 아닐 때와 맨위일 때를 if/else문으로 설정
+    if (nowScrollTop > 0) {
+      // 2-5 스크롤이 맨 위가 아닐 때 스크롤 방향을 판단하여 클래스 부여 및 제거
+      // 함수로 만든 wheelMove를 가져와 스크롤의 방향을 판단.
+      if (wheelMove() === "up") {
+        // 스크롤 방향이 위라면 "up" -> 위로 스크롤하고 있다면 scroll-down 클래스 삭제
+        document.documentElement.classList.remove("scroll-down");
+      } else {
+        // 스크롤 방향이 아래라면 "down" -> 아래로 스크롤하고 있다면 scroll-down 클래스 부여
+        document.documentElement.classList.add("scroll-down");
+      }
+
+      prevScrollTop = nowScrollTop;
+      console.log(nowScrollTop, prevScrollTop);
+    } else {
+      // 스크롤이 맨 위라면 클래스 삭제
+      document.documentElement.classList.remove("scroll-down");
+    }
+
     // 일단 이렇게 하면 스크롤값에 따라 active를 추가하긴 하지만, 자연스러운 새로고침을 통해서 나온다. -> 부자연스러운 효과를 구현
     // if 문을 통해서 스크롤 값이 0보다 클 때를 만들어준다
     // 현재 스크롤값과 이전 스크롤 값을 비교하여 방향을 판단.
     // > 0을 붙인 이유는 방향을 알 기 위해서 이다. 양수와 음수로 방향을 판단.
     // 아직 이렇게만 구현하면 스크롤이 위로 아래든 똑같이 active 클래스가 구현된다.
     // 아직 방향을 판단하는 코드가 없으면 스크롤 방향을 판단하지 못한다는 것이다.
-    if (prevScrollTop - nowScrollTop > 0) {
-      // btnTop을 class를 기입해서 구현, Jquery처럼 show 가능 이런 게 없음
-      // btnTop.classList.remove("active");
-    } else if (prevScrollTop - nowScrollTop < 0) {
-      // btnTop.classList.add("active");
-    }
+    // if (prevScrollTop - nowScrollTop > 0) {
+    //   btnTop을 class를 기입해서 구현, Jquery처럼 show 가능 이런 게 없음
+    //   btnTop.classList.remove("active");
+    // } else if (prevScrollTop - nowScrollTop < 0) {
+    //   btnTop.classList.add("active");
+    // }
   });
 
   // 2. top 버튼 클릭 시 홈페이지 맨 위로 이동
